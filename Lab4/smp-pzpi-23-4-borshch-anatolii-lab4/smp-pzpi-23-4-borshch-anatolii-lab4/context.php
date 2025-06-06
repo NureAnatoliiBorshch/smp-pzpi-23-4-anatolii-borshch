bf64ba6c304ca0c42bcd84a9ba291106b50028db
NureBorshchAnatolii <anatolii.borshch@nure.ua>
2025-06-07 00:09:36 +0300

    Delete Lab4/smp-pzpi-23-4-borshch-anatolii-lab4 directory

--- Lab4/smp-pzpi-23-4-borshch-anatolii-lab4/smp-pzpi-23-4-borshch-anatolii-lab4/context.php
+++
@@ -1,7 +0,0 @@
-<?php
-function getDbConnection(): PDO {
-    $db = new PDO('sqlite:' . __DIR__ . '/store.db');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $db;
-}
-?>
