b56c746c560e51b5c48978267fc31c869ceea4e9
NureBorshchAnatolii <anatolii.borshch@nure.ua>
2025-06-06 21:50:21 +0300

    Add files via upload

---
+++ Lab4/smp-pzpi-23-4-borshch-anatolii-lab4/smp-pzpi-23-4-borshch-anatolii-lab3/pages/static/footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-light py-3 mt-5">
+    <div class="text-center">
+        <a href="/home" class="text-decoration-none mx-4">Home</a> |
+        <a href="/products" class="text-decoration-none mx-4">Products</a> |
+        <a href="/cart" class="text-decoration-none mx-4">Cart</a> |
+        <a href="https://www.php.net/" class="text-decoration-none mx-4">About us</a>
+    </div>
+</footer>
